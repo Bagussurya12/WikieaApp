fb41b38bb84627721f53049d10040878ce46e260
bagussurya12 <bagus.suryaindrajati@gmail.com>
2023-10-24 21:05:00 +0700

    Add Discovery Screen

--- src/view/screens/home/DiscoverScreen.js
+++ src/view/screens/home/DiscoverScreen.js
@@ -1,8 +1,32 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, useWindowDimensions, Linking, Platform } from "react-native";
 import React from "react";
-import MapView, { Polyline } from "react-native-maps";
+import MapView from "react-native-maps";
+import Carousel from "react-native-reanimated-carousel";
+import { Card, Text, IconButton } from "react-native-paper";
+import { Ionicons } from "react-native-vector-icons";
 
 export default function DiscoverScreen() {
+  const markers = [
+    {
+      cordinate: { latitude: 37.8025259, longitude: -122.4351431 },
+      title: "Wikea Drop Store",
+      address: "1234 Main St, San Frasisco, CA 94122",
+      image: "https://plus.unsplash.com/premium_photo-1672136336540-2dd39fd4d1e2?auto=format&fit=crop&q=80&w=1856&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      cordinate: { latitude: 37.7896386, longitude: -122.421646 },
+      title: "Wikea Drop Store 2",
+      address: "1234 Main St, San Frasisco, CA 94122",
+      image: "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      cordinate: { latitude: 37.7665248, longitude: -122.4161628 },
+      title: "Wikea Drop Store 2",
+      address: "1234 Main St, San Frasisco, CA 94122",
+      image: "https://images.unsplash.com/photo-1634712282287-14ed57b9cc89?auto=format&fit=crop&q=80&w=1812&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
+  const { width } = useWindowDimensions();
   return (
     <View style={StyleSheet.absoluteFillObject}>
       <MapView
@@ -13,19 +37,40 @@ export default function DiscoverScreen() {
           longitudeDelta: 0.0421,
         }}
         style={StyleSheet.absoluteFillObject}
-      >
-        <Polyline
-          coordinates={[
-            { latitude: 37.8025259, longitude: -122.4351431 },
-            { latitude: 37.7896386, longitude: -122.421646 },
-            { latitude: 37.7665248, longitude: -122.4161628 },
-            { latitude: 37.7734153, longitude: -122.4577787 },
-            { latitude: 37.8025259, longitude: -122.4351431 },
-          ]}
-          strokeWidth={10}
-          fillColor="#00f"
-        ></Polyline>
-      </MapView>
+      />
+      <View style={{ alignItems: "center" }}>
+        <Carousel
+          loop={false}
+          width={width - 30}
+          height={250}
+          autoPlay
+          data={markers}
+          style={{ top: 20 }}
+          scrollAnimationDuration={3000}
+          renderItem={({ item }) => (
+            <Card style={{ overflow: "hidden" }}>
+              <Card.Cover source={{ uri: item.image }} style={{ height: 150, borderRadius: 0 }} />
+              <Card.Content style={{ padding: 16, flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+                <View>
+                  <Text style={{ fontSize: 18, fontWeight: "500" }}>{item.title}</Text>
+                  <Text>{item.address}</Text>
+                </View>
+                <IconButton
+                  mode="outlined"
+                  onPress={() =>
+                    Linking.openURL(
+                      Platform.OS === "android"
+                        ? `google.navigation:q=${item.cordinate.latitude},${item.cordinate.longitude}`
+                        : `maps://app?daddr=${item.cordinate.latitude}, ${item.cordinate.longitude}`
+                    )
+                  }
+                  icon={() => <Ionicons size={24} name="locate" />}
+                ></IconButton>
+              </Card.Content>
+            </Card>
+          )}
+        />
+      </View>
 
       <View style={{ position: "absolute", top: 100, left: 50 }} />
     </View>
